2b5091ef7a3619a55c6fe8b767c75359e7edd6b1
TaDa37 <pabloa.desantiago@gmail.com>
2023-06-25 00:26:22 -0500

    Update streamlit_app.py
    Fixed emojis location

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
    
-🥣streamlit.header('Breakfast Menu')
-🥗streamlit.text('Omega 3 & Blueberry Oatmeal')
-🐔streamlit.text('Kale, Spinach & Rocket Smoothie')
-🥑🍞streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
